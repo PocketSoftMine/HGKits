bb795eb6287385bc638f5a95e7df2acf367e3422
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-04-21 13:06:02 -0300

    Update Loader.php

--- specialkits/src/SpecialKits/Loader.php
+++ specialkits/src/SpecialKits/Loader.php
@@ -10,9 +10,9 @@ use pocketmine\utils\TextFormat;
 class Loader extends PluginBase{
   public function onEnable(){
     
-		$this->saveDefaultConfig();
+	$this->saveDefaultConfig();
     
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getLogger()->info(color::YELLOW. "HGKITS-SpecialKits Ligado!");
+	$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	$this->getServer()->getLogger()->info(color::YELLOW. "HGKITS-SpecialKits Ligado!");
   }
 }
